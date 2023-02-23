c134f7c1454c1324f2abc03179bbd4d8cc83581c
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2023-02-23 14:12:50 -0600

    updated seeding to insert messages correctly

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../src/server/db';
 
 async function main() {
-  const defaultUser = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: {
       id: 'cle8zst2e0230ypfn3pew738s',
     },
@@ -9,36 +9,31 @@ async function main() {
       id: 'cle8zst2e0230ypfn3pew738s',
       name: 'John Doe',
       email: 'john@mail.com',
-      password: 'qwer1234',
-    },
-    update: {},
-  });
-
-  await prisma.chat.upsert({
-    where: {
-      id: 'clef9a8y33401ypctjma6uv3j',
-    },
-    create: {
-      id: 'clef9a8y33401ypctjma6uv3j',
-      userId: defaultUser.id,
-      messages: {
+      password: 'adminadmin',
+      chats: {
         create: [
           {
-            message: 'What have you been up to?',
-            isBot: false,
-          },
-          {
-            message: 'Watching old movies.',
-            isBot: true,
-          },
+            messages: {
+              create: [
+                {
+                  message: 'What have you been up to?',
+                  isBot: false,
+                },
+                {
+                  message: 'Watching old movies.',
+                  isBot: true,
+                },
 
-          {
-            message: 'Did you watch anything interesting?',
-            isBot: false,
-          },
-          {
-            message: 'Yes, I watched Casablanca. It was really great!',
-            isBot: true,
+                {
+                  message: 'Did you watch anything interesting?',
+                  isBot: false,
+                },
+                {
+                  message: 'Yes, I watched Casablanca. It was really great!',
+                  isBot: true,
+                },
+              ],
+            },
           },
         ],
       },
